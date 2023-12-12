5c7298ba0f6d9586fcddf44a1f6ebce21d73e3b4
JBasias <jbasias@gmail.com>
2023-12-12 18:13:10 -0500

    needed

--- island.js
+++ island.js
@@ -1,5 +1,26 @@
-function getNeighbors(row, col, matrix) {
-  
+function getNeighbors(x, y, G) {
+
+
+
+  let n= G.length; let m=G[0].length;
+
+  let ret= [];
+
+  if(x>0 && G[x-1][y] === 1) ret.push([x-1,y]);
+  if(x<n-1  && G[x+1][y] === 1) ret.push([x+1,y]);
+  if(y>0 && G[x][y-1] === 1) ret.push([x,y-1]);
+  if(y<m-1 && G[x][y+1] === 1) ret.push([x,y+1]);
+
+  if(x>0 && y>0 && G[x-1][y-1] === 1) ret.push([x-1,y-1]);
+  if(x>0 && y<m-1 && G[x-1][y+1] === 1) ret.push([x-1,y+1]);
+  if(x<n-1 && y>0 && G[x+1][y-1] === 1) ret.push([x+1 ,y-1]);
+  if(x<n-1 && y<m-1 && G[x+1][y+1] === 1) ret.push([x+1,y+1]);
+
+
+  //console.log(ret);
+
+  return ret;
+
   // Check top
   // Check top right
   // Check right
@@ -9,16 +30,53 @@ function getNeighbors(row, col, matrix) {
   // Check left
   // Check top left
   // Return neighbors
-  
+
   // Your code here
 }
 
-function countIslands(matrix) {
-  
+function countIslands(M) {
+
+  let B = new Set(); let m=M[0].length; let ret=0;
+
+  function Find(x,y,M)
+  {
+    B.add(x*m + y);
+    let Q = [[x,y]]; let cur;
+
+    while(Q.length != 0 )
+    {
+       cur = Q.shift();
+       for(v of getNeighbors(cur[0],cur[1],M))
+       {
+         if(!B.has(v[0]*m + v[1]))
+         {
+           B.add(v[0]*m + v[1]);
+           Q.push(v);
+         }
+       }
+
+    }
+  }
+
+
+  for(let i=0;i<M.length;i++)
+  {
+    for(let j=0;j<M[0].length;j++)
+    {
+      if(M[i][j] === 1  && !B.has(i*m + j))
+      {
+          ret++;
+          Find(i,j,M);
+      }
+    }
+  }
+
+  return ret;
+
   // Create a visited set to store visited nodes
   // Initialize count to 0
   // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
+    // If an index contains a 1 and has not been visited,
     // increment island count and start traversing neighbors
       // DO THE THING (increment island count by 1)
       // Initialize a stack with current index
@@ -31,7 +89,7 @@ function countIslands(matrix) {
             // Add neighbor to stack
             // Mark neighbor as visited
   // Return island count
-  
+
   // Your code here
 }
 
